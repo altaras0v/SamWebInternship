1edf59b2ef0385039ad64781bbc2a04e51b4c772
Alexis <altaras0b@gmail.com>
2019-05-28 14:23:53 +0300

    add to github

---
+++ src/main/java/myapp/AppConfig.java
@@ -0,0 +1,83 @@
+package myapp;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class AppConfig {
+    //private Properties dataSourceProperties;
+
+    @Bean
+    @DependsOn("flyway")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[]{"myapp"});
+
+       JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+
+        return em;
+    }
+    @Bean
+    public DataSource dataSource(){
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/elearning?characterEncoding=latin1&useConfigs=maxPerformance");
+        dataSource.setUsername( "root" );
+        dataSource.setPassword( "admin" );
+        return dataSource;
+    }
+
+    @Bean(initMethod = "migrate")
+    Flyway flyway() {
+        Flyway flyway = Flyway
+                .configure()
+                .dataSource(dataSource())
+                .baselineOnMigrate(true)
+                .load();
+        //flyway.migrate();
+
+/*
+        Flyway flyway = new Flyway();
+        flyway.setBaselineOnMigrate(true);
+        return flyway;
+*/
+        return flyway;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        //properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+
+        return properties;
+    }
+}
