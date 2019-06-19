d45695d2f102de1dbf594e116994654f2695d0e2
Alexis <altaras0b@gmail.com>
2019-06-19 14:44:30 +0300

    rework file uploader name autodetection (test version)

--- src/main/java/myapp/controller/FileUploadController.java
+++ src/main/java/myapp/controller/FileUploadController.java
@@ -24,12 +24,15 @@ public class FileUploadController {
      */
     @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
     public @ResponseBody
-    String uploadFileHandler(@RequestParam("name") String name,
-                             @RequestParam("file") MultipartFile file) {
+    String uploadFileHandler(@RequestParam("file") MultipartFile file) {
+
+        String name = null;
 
         if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
+
+                name = file.getOriginalFilename();
 
                 // Creating the directory to store file
                 String rootPath = System.getProperty("catalina.home");
@@ -58,47 +61,4 @@ public class FileUploadController {
         }
     }
 
-    /**
-     * Upload multiple file using Spring Controller
-     */
-    @RequestMapping(value = "/uploadMultipleFile", method = RequestMethod.POST)
-    public @ResponseBody
-    String uploadMultipleFileHandler(@RequestParam("name") String[] names,
-                                     @RequestParam("file") MultipartFile[] files) {
-
-        if (files.length != names.length)
-            return "Mandatory information missing";
-
-        String message = "";
-        for (int i = 0; i < files.length; i++) {
-            MultipartFile file = files[i];
-            String name = names[i];
-            try {
-                byte[] bytes = file.getBytes();
-
-                // Creating the directory to store file
-                String rootPath = System.getProperty("catalina.home");
-                File dir = new File(rootPath + File.separator + "tmpFiles");
-                if (!dir.exists())
-                    dir.mkdirs();
-
-                // Create the file on server
-                File serverFile = new File(dir.getAbsolutePath()
-                        + File.separator + name);
-                BufferedOutputStream stream = new BufferedOutputStream(
-                        new FileOutputStream(serverFile));
-                stream.write(bytes);
-                stream.close();
-
-                logger.info("Server File Location="
-                        + serverFile.getAbsolutePath());
-
-                message = message + "You successfully uploaded file=" + name
-                        + "<br />";
-            } catch (Exception e) {
-                return "You failed to upload " + name + " => " + e.getMessage();
-            }
-        }
-        return message;
-    }
 }
--- src/main/webapp/WEB-INF/pages/upload.jsp
+++ src/main/webapp/WEB-INF/pages/upload.jsp
@@ -15,7 +15,6 @@
 <body>
 <form method="POST" action="uploadFile" enctype="multipart/form-data">
     File to upload: <input type="file" name="file"><br />
-    Name: <input type="text" name="name"><br /> <br />
     <input type="submit" value="Upload"> Press here to upload the file!
 </form>
 </body>
