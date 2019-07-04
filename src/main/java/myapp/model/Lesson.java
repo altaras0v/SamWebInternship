5a67fd0622f912a24173fae29a5472a16ea78b4e
Alexis <altaras0b@gmail.com>
2019-07-04 16:26:36 +0300

    Add Lesson entity and Lesson DTO

---
+++ src/main/java/myapp/model/Lesson.java
@@ -0,0 +1,51 @@
+package myapp.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "lesson")
+public class Lesson {
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Lesson(){
+
+    }
+
+    public Lesson(int id,String name,String description){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
